47cc16e65e04d6f597bb246302041956939007ae
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-02-01 14:25:42 +0900

    [React] Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -4,28 +4,22 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const Text = styled.span`
-  color: #fff;
+//extends 방법
+const Circle = styled(Box)`
+  border-radius: 50px;
 `;
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text> Hello</Text>
-      </BoxOne>
-      <BoxTwo> </BoxTwo>
+      <Box bgColor="teal"></Box>
+      <Circle bgColor="blue"></Circle>
     </Father>
   );
 }
