c011124af076ffcd8118d5695967ed4f29d7ea5d
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-02-01 14:40:44 +0900

    [React] As and Attrs

--- src/App.js
+++ src/App.js
@@ -4,22 +4,26 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Btn = styled.button`
+  color: #fff;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
 `;
 
-//extends 방법
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal"></Box>
-      <Circle bgColor="blue"></Circle>
+    <Father as="header">
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   );
 }
